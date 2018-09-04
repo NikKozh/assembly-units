4ff8e6fe7b72bab007aaef91453533830a01fd61
zitt <NKtargeting@yandex.ru>
2018-09-04 12:39:39 +0500

    Minor change the assemly unit

--- src/ru/nikkozh/assemblyUnits/models/AssemblyUnit.java
+++ src/ru/nikkozh/assemblyUnits/models/AssemblyUnit.java
@@ -4,22 +4,32 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class AssemblyUnit {
-  private final String name;
+  private int id;
+  private String name;
   private Map<String, Integer> parts = new TreeMap<>();
   
-  public AssemblyUnit(String name) {
+  public void setName(String name) {
     this.name = name;
   }
-
+  
   public String getName() {
     return name;
   }
 
-  public Map<String, Integer> getParts() {
+  public Map<String, Integer> getParts() { 
     return parts;
   }
 
-  public void setPart(String partName, int amountParts) {
-    // TODO: заглушка
+  public void putPart(String partName, int partAmount) {
+    // TODO: сделать проверку на существование данных деталей и суммирование в таком случае их количества
+    parts.put(partName, partAmount);
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
   }
 }
